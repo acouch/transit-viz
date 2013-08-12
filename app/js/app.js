3bde703c197607adb1dc1dd506db580de707c974
acouch <acinternets@gmail.com>
2013-08-12 07:51:56 -0500

    Fixing index-async and adding callback for route clicks

--- app/js/app.js
+++ app/js/app.js
@@ -3,22 +3,20 @@
 angular.module('inequalityTransitMap', []);
 function CitiesCtrl($scope, $location) {
 
-  // Set initial Defaults.
+	// Set initial Defaults.
 	$scope.cities = cities;
-  $scope.censusPoints = censusPoints;
-  // Set initial Defaults.
-  $scope.currentCity = $scope.cities[0];
-  $scope.currentDataPoint = $scope.censusPoints[4];
+	$scope.censusPoints = censusPoints;
+	$scope.currentCity = $scope.cities[0];
+	$scope.currentDataPoint = $scope.censusPoints[4];
 
-  // Override defaults if path is set.
-  if ($location.$$path) {
-    var path = $location.$$path.split('/');
-    console.log(path);
-    var cityKey = getKey($scope.cities, path[1]);
-    var censusKey = getKey(censusPoints, path[2]);
-    $scope.currentCity = $scope.cities[cityKey];
-    $scope.currentDataPoint = $scope.censusPoints[censusKey];
-  }
+	// Override defaults if path is set.
+	if ($location.$$path) {
+		var path = $location.$$path.split('/');
+		var cityKey = getKey($scope.cities, path[1]);
+		var censusKey = getKey(censusPoints, path[2]);
+		$scope.currentCity = $scope.cities[cityKey];
+		$scope.currentDataPoint = $scope.censusPoints[censusKey];
+	}
 
 	var map = mapTool.init({
 		lat: $scope.currentCity.lat,
@@ -28,17 +26,30 @@ function CitiesCtrl($scope, $location) {
 		currentDataPoint: $scope.currentDataPoint,
 	});
 
-  $scope.dataSelect = function() {
-    $location.path('/' + $scope.currentCity.value + '/' + $scope.currentDataPoint.value);
+  // Respond to change in city or data point.
+	$scope.dataSelect = function() {
+		$location.path('/' + $scope.currentCity.value + '/' + $scope.currentDataPoint.value);
 		mapTool.update({
 			lat: $scope.currentCity.lat,
 			lon: $scope.currentCity.lon,
 			zoom: $scope.currentCity.zoom,
 			dataPath:  $scope.currentCity.dataPath,
 			currentDataPoint: $scope.currentDataPoint,
-    });
+		});
 		mapTool.execute();
-  };
+	};
 
-  mapTool.execute();
+  // Respond to route click.
+	$scope.routeSelect = function() {
+		$location.path('/' + $scope.currentCity.value + '/' + $scope.currentDataPoint.value);
+		mapTool.update({
+			lat: $scope.currentCity.lat,
+			lon: $scope.currentCity.lon,
+			zoom: $scope.currentCity.zoom,
+		});
+		mapTool.execute();
+	};
+
+
+	mapTool.execute();
 }
--- app/js/mapTool.js
+++ app/js/mapTool.js
@@ -12,41 +12,41 @@
 		zoom: 5,
 		censusToken: 'f5bd9c51b563f034639bab7be5bb546c1b456cdc',
 		mapDiv: "",
-    currentDataPoint: "",
+		currentDataPoint: "",
 		dataPath: "",
 	}
 
 	mapTool.init = function(settings) {
 		// Options.
 		options = $.extend(mapTool.settings, settings);
-		console.log('buts party');
-		console.log(options.lat);
 		var cloudmadeUrl = options.cloudmadeUrl,
 			cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: options.cloudmadeZoom, attribution: options.cloudmadeAttribution});
 
-    var map = mapTool.createMap(options.lat, options.lon, cloudmade);
-    options.map = map;
+		var map = mapTool.createMap(options.lat, options.lon, cloudmade);
+		options.map = map;
 		return map;
 	}
 
-  mapTool.update = function(updates) {
+	mapTool.update = function(updates) {
 		options = $.extend(options, updates);
-  }
+	}
   
-  mapTool.execute = function() {
+	mapTool.execute = function() {
 
 		// TODO: separate as a promise.
 		d3.csv(options.dataPath, function(error, stops_data) {
-			mapTool.centerMap(options.map, options.lat, options.lon);
-			mapTool.addData(options.map, stops_data, options.lat, options.lon, options.currentDataPoint);
+			mapTool.addData(stops_data);
+			mapTool.centerMap();
+			mapTool.addRoutes(stops_data);
+			mapTool.addPoints();
 			//createGraph(stops_data, $scope);
 		});
-  }
+	}
 
-	mapTool.centerMap = function(map, lat, lon) {
-		map.panTo(new L.LatLng(lat, lon)); 
-	  // TODO: Fit to bounds.
-	  return map;
+	mapTool.centerMap = function() {
+		options.map.panTo(new L.LatLng(options.lat, options.lon)); 
+		// TODO: Fit to bounds.
+		return map;
 	}
 
 	mapTool.createMap = function(lat, lon, cloudmade) {
@@ -59,29 +59,33 @@
 		return map;
 	}
 
-  mapTool.addData = function(map, stops_data, lat, lon, currentDataPoint) {
-
+	mapTool.addData = function(stops_data) {
+    $("g").remove();
 		var svg = d3.select("#map").select("svg"),
 			g = svg.append("g");
-
+    options.g = g;
 		var feature = g.selectAll("circles.points")
 			.data(stops_data)
 			.enter()
 			.append("circle")
 			.attr("r",5)
 			.attr("class", function(d) { return "route_" + d.route_id;})
-			.attr("cx",function(d) { latLon = new L.LatLng(d.stop_lat, d.stop_lon); return map.latLngToLayerPoint(latLon).x})
-			.attr("cy",function(d) { latLon = new L.LatLng(d.stop_lat, d.stop_lon); return map.latLngToLayerPoint(latLon).y})
+			.attr("cx",function(d) { latLon = new L.LatLng(d.stop_lat, d.stop_lon); return options.map.latLngToLayerPoint(latLon).x})
+			.attr("cy",function(d) { latLon = new L.LatLng(d.stop_lat, d.stop_lon); return options.map.latLngToLayerPoint(latLon).y})
 			.attr("style", function(d) { return "fill: #" + d.route_color; });
 
-		map.on("viewreset", update);
-		function update() {
-			feature.attr("cx",function(d) { latLon = new L.LatLng(d.stop_lat, d.stop_lon); return map.latLngToLayerPoint(latLon).x})
-			feature.attr("cy",function(d) { latLon = new L.LatLng(d.stop_lat, d.stop_lon); return map.latLngToLayerPoint(latLon).y})
+		options.map.on("viewreset", attach);
+		function attach() {
+			feature.attr("cx",function(d) { latLon = new L.LatLng(d.stop_lat, d.stop_lon); return options.map.latLngToLayerPoint(latLon).x})
+			feature.attr("cy",function(d) { latLon = new L.LatLng(d.stop_lat, d.stop_lon); return options.map.latLngToLayerPoint(latLon).y})
 		}
+		attach();
+	}
+  
+  mapTool.addPoints = function() {
 
-		update();
-
+    g = d3.select("g");
+    //g = options.g;
 		g.selectAll("circle")
 			.on("mouseover", function(d) {
 				d3.select(this)
@@ -104,14 +108,14 @@
 					var stateFip = FIPS.substring(0,2);
 					var countyFip = FIPS.substring(2,5);
 					var tractFip = FIPS.substring(5,11);
-					var censusUrl = 'http://api.census.gov/data/2011/acs5?key=' + options.censusToken + '&get=' + currentDataPoint.value + ',NAME&for=block+group:1&in=state:' + stateFip + '+county:' + countyFip + '+tract:' + tractFip;
+					var censusUrl = 'http://api.census.gov/data/2011/acs5?key=' + options.censusToken + '&get=' + options.currentDataPoint.value + ',NAME&for=block+group:1&in=state:' + stateFip + '+county:' + countyFip + '+tract:' + tractFip;
 					$.getJSON(censusUrl, function(data) {
 						d3.select("#tooltip")
 							.style("left", (pageX) + 20 + "px")
 							.style("top", (pageY) - 30 + "px")
 							.style("display", "block")
 							.transition()
-							.text(d.route_short_name + ': ' + d.stop_name + ' ' + currentDataPoint.name + ': ' + data[1][0]);
+							.text(d.route_short_name + ': ' + d.stop_name + ' ' + options.currentDataPoint.name + ': ' + data[1][0]);
 					});
 				});
 		});
@@ -123,20 +127,42 @@
 					.attr("r", 5);
 				$('#tooltip').hide();
 		});
+	}
+
+  mapTool.addRoutes = function(stops_data) {
 		var routes = {};
 		// Lines
-		$.each(stops_data, function(key, value){
-			routes[value.route_id] = {name: value.route_short_name, color: value.route_color};
+		$.each(stops_data, function(key, value) {
+      if ( Object.prototype.toString.call( routes[value.route_id] ) === '[object Object]' ) {
+				routes[value.route_id].stops[value.stop_id] = value;
+			} else {
+				routes[value.route_id] = {name: value.route_short_name, color: value.route_color, stops: {}};
+			}
 		});
 		$('#routes ul').empty();
 		$.each(routes, function(id, items) {
 			$('#routes ul').append('<li style="background-color: #' + items.color + '"><a href="#' + id  + '">' + items.name + '</a></li>');
 		});
+    console.log(routes);
 
-		// Action.
+		// TODO: switch to ng-model
 		d3.selectAll("#routes ul li a")
-			.on("click", function(d) { moveItMoveIt(d3.select(this).attr("href"), stops_data)});
+			.on("click", function(d) { mapTool.activateRoute(d3.select(this).attr("href").substring(1), routes);});
 	}
+  
+  mapTool.activateRoute = function(route, routes) {
+    var arrayOfLatLons = [];
+    var x = 0;
+		$.each(routes[route].stops, function(key, value) {
+      arrayOfLatLons[x] = [value.stop_lat, value.stop_lon];
+      x++;
+    });
+    var bounds = new L.LatLngBounds(arrayOfLatLons);
+    options.map.fitBounds(bounds);
+   
+		return;
+    updateGraph(route);
+  }
   
 })();
 
