7810365a878a75afc29b44b31f987aa0b75828ba
nuamsmachine <developer@nuams.com>
2014-10-21 08:53:58 -0400

    Updates to msp

--- app/js/data.js
+++ app/js/data.js
@@ -3,7 +3,7 @@ var cities = [
 	{name:'Philadelphia - bus', value:'phl-bus', dataPath: 'data/septa_bus_gtfs/routes_stops.csv', lon: '-75.205219', lat: '40.00061', zoom: '10' },
 	{name:'Puerto Rico', value:'pr', dataPath: 'data/pr_gtfs/routes_stops.csv', lon: '-66.590149', lat: '18.220833', zoom: '10' },
 	{name:'Tucson', value:'tucson', dataPath: 'data/tucson_gtfs/routes_stops.csv', lon: '-110.977224', lat: '32.216585', zoom: '10' },
-	{name:'Minneapolis', value:'mps', dataPath: 'data/mps_gtfs/routes_stops.csv', lon: '-93.266670', lat: '44.983334', zoom: '10' },
+	{name:'Minneapolis', value:'msp', dataPath: 'data/msp_gtfs/routes_stops.csv', lon: '-93.266670', lat: '44.983334', zoom: '10' },
 	{name:'Madison', value:'mds', dataPath: 'data/madison_gtfs/routes_stops.csv', lon: '-89.4011', lat: '43.0731', zoom: '12'},]
 
 var censusPoints = [
